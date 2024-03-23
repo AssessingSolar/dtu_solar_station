837bb7302d3ce2e2a355dc9cf24dc93793bf78d2
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2024-03-23 22:17:15 +0100

    Update table

--- scripts/generate_data_list.py
+++ scripts/generate_data_list.py
@@ -14,57 +14,25 @@ months = month_years.dt.strftime('%b').unique()
 
 url = "https://raw.githubusercontent.com/AssessingSolar/dtu_solar_station/main/"
 
-lines = []
-for y in years:
-    lines.append("")
-    lines.append(f"## {y}")
-    for m in months:
-        month_number = pd.to_datetime(m, format='%b').strftime('%m')
-        filename = f"dtu_{y}_{month_number}.csv"
-        if filename in file_basenames:
-            l = f"- [{m} {y}]({url}data/{filename})"
-        lines.append(l)
-
-with open('docs/data.txt', 'w') as f:
-    for line in lines:
-        f.write(line)
-        f.write('\n')
-
-# %%
 header = '|   |' + ''.join([f" {m} |" for m in months])
 
 separator = ''.join(['|---']*(len(months)+1)) + '|'
 
 lines = [header, separator]
+
 for y in years:
     line = f"| {y} |"
     for m in months:
         month_number = pd.to_datetime(m, format='%b').strftime('%m')
         filename = f"dtu_{y}_{month_number}.csv"
         if filename in file_basenames:
-            text = f"{m} {y}"
-            link = os.path.join([url, filename])
-            line += f" [{text}]({link}) |"
+            link = os.path.join(url, filename)
+            line += f" [link]({link}) |"
         else:
-            line += "        "
+            line += " |"
     lines.append(line)
 
-lines.append(separator)
-
-# %%
-    year_files = sorted(glob.glob(f'data/dtu_{y}*.csv'))
-    
-    for f in year_files:
-        m = year_files
-        link = os.path.join([url, f])
-        text = 
-        
-    
-
-    
-    month_number = pd.to_datetime('Feb', format='%b').strftime('%m')
-    filename = f"dtu_{y}_{month_number}.csv"
-
-
-    line = f'| {y} |' + ''.join([f' [link]({m}) |' for m in months])
-    lines.append(line)
+with open('docs/data.txt', 'w') as f:
+    for line in lines:
+        f.write(line)
+        f.write('\n')
