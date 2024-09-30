24ee8ed98e4777f14d6ba4b2aa2ace7c507d3b81
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2024-09-30 10:58:17 +0200

    Revert "Add three decimal place rounding"
    This reverts commit 754c24caed395b63dab9059a6a5e922e8a8a8ba4.

--- scripts/generate_data_files.py
+++ scripts/generate_data_files.py
@@ -4,7 +4,7 @@ latitude_deg_dtu = 55.790697
 longitude_deg_dtu = 12.525087
 elevation_dtu = 85
 
-start_date = pd.Timestamp('2024-06-01')
+start_date = pd.Timestamp('2022-03-01')
 end_date = pd.Timestamp.today().round('1d') - pd.offsets.MonthEnd() + pd.Timedelta(minutes=24*60-1)
 
 df_import = load_weather_data(file_path_raw_weather, start_date, end_date)
@@ -25,5 +25,4 @@ months = pd.date_range(start=df_raw.index[0], end=df_raw.index[-1], freq='MS')
 for m in months:
     m_end = m + pd.offsets.MonthEnd() + pd.Timedelta(minutes=24*60-1)
     df_sub = df_raw.loc[m: m_end, parameters+rains].asfreq('1min')
-    df_sub = df_sub.round(3)
     df_sub.to_csv(f"data/dtu_{m.strftime('%Y_%m')}.csv")
